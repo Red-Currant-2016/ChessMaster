58381eb2862ae31a6082db4bcf28979fa2e4e1d3
Martin Kamenov <martykam@abv.bg>
2016-12-28 19:43:50 +0200

    Created Chess Board

--- ChessMaster2017/ChessMaster2017/Form1.cs
+++ ChessMaster2017/ChessMaster2017/Form1.cs
@@ -10,11 +10,13 @@ using System.Windows.Forms;
 
 namespace ChessMaster2017
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
+
+        
     }
 }
