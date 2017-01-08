4d36bc8b9da03f620085e64786e6f801d5ca1ae1
Martin Kamenov <martykam@abv.bg>
2017-01-08 13:58:13 +0200

    Testing units

--- ChessMaster2017/ChessMaster2017/Form1.cs
+++ ChessMaster2017/ChessMaster2017/Form1.cs
@@ -11,23 +11,48 @@ using System.Windows.Forms;
 
 namespace ChessMaster2017
 {
+    
     public partial class MainForm : Form
     {
+        
+        bool Action = false;
+        Board testBoard = new Board();
         public MainForm()
         {
             InitializeComponent();
                        
-            Board testBoard = new Board();
-            e5.Image = Properties.Resources.black_pawn;
+            
+            /*e5.Image = Properties.Resources.black_pawn;
             e7.Image = null;
-            b2.Image = e5.Image;
+            b2.Image = e5.Image;*/
         }
 
         private void Selector(object sender, EventArgs e)
         {
-            /*Control control = (Control)sender;
-            MessageBox.Show(control.Name);*/
+            int x = 0;
+            int y = 0;
+            PictureBox control = (PictureBox)sender;
+            string cordinates = control.Name;
+            x = cordinates[0] - 'a';
+            y = cordinates[1] - '1';
+            if (!Action)
+            {
 
+                /*Control control = (Control)sender;
+                MessageBox.Show(control.Name);*/
+                 
+                bool isSelected = testBoard.SelectChessPiece(x, y);
+                if (isSelected)
+                {
+                    Action = true; ;
+                }
+                //control.BackColor = Color.Aqua;
+            }
+            else
+            {
+                testBoard.MoveSelectedChessPiece(x, y);
+                Action = false;
+            }
         }
     }
 }
