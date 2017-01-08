6a81f198ab8f7b3db468060a75a859dc2720f56a
petk0che <Cvety@CVETI>
2017-01-08 18:01:12 +0200

    Finished Bishop, Summery written for Bishop, the same logic may be applied to all other figures

--- ChessMaster2017/ChessMaster2017/BackEnd/pieces/Bishop.cs
+++ ChessMaster2017/ChessMaster2017/BackEnd/pieces/Bishop.cs
@@ -12,7 +12,29 @@ namespace ChessMaster2017.BackEnd
         {
         }
 
-       
+        /// <summary>
+        /// The method Possible Moves takes "CurrentBoard" to see which squares are available and it applies logic to define
+        /// the possible squares for moves as true of false.
+        /// 
+        /// The logic
+        /// 
+        /// Lets assume that bishop is white ( ChessPieceColor BishopColor = this.Color; ). 
+        /// The Bishop can only move to its diagonals. The logic is the same for all four.
+        /// Diagonal 1 up left diagonal - x increases and y increases
+        /// we have 3 conditions :
+        /// 1 - the space is taken by a figure with the same color - then this space is market as false, the cycle breaks and
+        /// the logic starts checking the other 3 diagonals for possible moves.
+        /// 
+        /// 2-the space is taken but the figure is from the oppossite color - then only an attack is possible, the space is marked
+        /// as true and the cycle breaks.
+        /// 
+        /// 3-the space is free, the cycle will continue to mark spots as free unless it meets condition 1 or 2.
+        ///
+        /// 
+        /// </summary>
+        /// <param name="currentBoard"></param>
+        /// <returns></returns>
+
 
         public override bool[,] PossibleMove(ChessPiece[,] currentBoard)
         {
@@ -58,7 +80,44 @@ namespace ChessMaster2017.BackEnd
                     }
                 }
 
-            // left right diagonal
+            // right up diagonal
+            for (int movesX = bishopX; movesX < 8; movesX++)
+            {
+                for (int movesY = bishopY; movesY < 8; movesY++)
+                {
+                    if (currentBoard[movesX, movesY] != null && currentBoard[movesX, movesY].Color == BishopColor)
+                    {
+                        bishopMoves[movesX, movesY] = false;
+                        exitCycle = true;
+                        break;
+                    }
+                    else if (currentBoard[movesX, movesY] != null && currentBoard[movesX, movesY].Color != BishopColor)
+                    {
+                        bishopMoves[movesX, movesY] = true;
+                        exitCycle = true;
+                        break;
+                    }
+
+                    else
+                    {
+                        bishopMoves[movesX, movesY] = true;
+                    }
+
+                }
+                if (exitCycle)
+                {
+                    exitCycle = false;
+                    break;
+
+                }
+            }
+
+
+
+            // left bottom diagonal
+
+            
+
             for (int movesX = bishopX; movesX < 8; movesX++)
             {
                 for (int movesY = bishopY; movesY < 8; movesY--)
@@ -90,7 +149,40 @@ namespace ChessMaster2017.BackEnd
                 }
             }
 
-            ChessPieceColor PieceColor =  currentBoard[1, 1].Color;
+            // right bottom diagonal
+
+           
+
+            for (int movesX = bishopX; movesX < 8; movesX--)
+            {
+                for (int movesY = bishopY; movesY < 8; movesY--)
+                {
+                    if (currentBoard[movesX, movesY] != null && currentBoard[movesX, movesY].Color == BishopColor)
+                    {
+                        bishopMoves[movesX, movesY] = false;
+                        exitCycle = true;
+                        break;
+                    }
+                    else if (currentBoard[movesX, movesY] != null && currentBoard[movesX, movesY].Color != BishopColor)
+                    {
+                        bishopMoves[movesX, movesY] = true;
+                        exitCycle = true;
+                        break;
+                    }
+
+                    else
+                    {
+                        bishopMoves[movesX, movesY] = true;
+                    }
+
+                }
+                if (exitCycle)
+                {
+                    exitCycle = false;
+                    break;
+
+                }
+            }
 
 
 
