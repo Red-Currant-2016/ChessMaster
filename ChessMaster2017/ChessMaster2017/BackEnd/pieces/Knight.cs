3bf19973faf15f21063d624466e25a087b9991c3
MartinStanchev23 <marchooo97.ms@gmail.com>
2017-01-09 15:31:31 +0200

    Implemented knight possible moves method.

--- ChessMaster2017/ChessMaster2017/BackEnd/pieces/Knight.cs
+++ ChessMaster2017/ChessMaster2017/BackEnd/pieces/Knight.cs
@@ -9,8 +9,168 @@ namespace ChessMaster2017.BackEnd
 {
     class Knight : ChessPiece
     {
+/*    -8-1-
+ *    7---2
+ *    --k--
+ *    6---3
+ *    -5-4-
+ */
         public Knight(int x, int y, ChessPieceColor color, ChessPieceType type) : base(x, y, color, type)
         {
+        }
+        public override bool[,] PossibleMove(ChessPiece[,] currentBoard)
+        {
+            int knightX = this.CurrentX;
+            int knightY = this.CurrentY;
+
+            bool[,] knightMoves = new bool[8, 8];
+            ChessPieceColor knightColor = this.Color;
+
+            //8 = top left
+            if (knightX + 2 < 8 && knightX + 2 >= 0 && knightY - 1 >= 0 && knightY - 1 < 8)
+            {
+                if (currentBoard[knightX + 2, knightY - 1] != null && currentBoard[knightX + 2, knightY - 1].Color == knightColor)
+                {
+                    knightMoves[knightX + 2, knightY - 1] = false;
+                }
+                else if (currentBoard[knightX + 2, knightY - 1] != null && currentBoard[knightX + 2, knightY - 1].Color != knightColor)
+                {
+                    knightMoves[knightX + 2, knightY - 1] = true;
+                }
+                else
+                {
+                    knightMoves[knightX + 2, knightY - 1] = true;
+                }
+            }
+
+
+            // 7 = left top
+
+            if (knightX + 1 < 8 && knightX + 1 >= 0 && knightY - 2 >= 0 && knightY - 2 < 8)
+            {
+                if (currentBoard[knightX + 1, knightY - 2] != null && currentBoard[knightX +1, knightY - 2].Color == knightColor)
+                {
+                    knightMoves[knightX + 1, knightY - 2] = false;
+                }
+                else if (currentBoard[knightX + 1, knightY - 2] != null && currentBoard[knightX + 1, knightY - 2].Color != knightColor)
+                {
+                    knightMoves[knightX + 1, knightY - 2] = true;
+                }
+                else
+                {
+                    knightMoves[knightX + 1, knightY - 2] = true;
+                }
+            }
+
+            // 6 = left bottom
+
+            if (knightX - 1 < 8 && knightX - 1 >= 0 && knightY - 2 >= 0 && knightY - 2 < 8)
+            {
+                if (currentBoard[knightX - 1, knightY - 2] != null && currentBoard[knightX - 1, knightY - 2].Color == knightColor)
+                {
+                    knightMoves[knightX - 1, knightY - 2] = false;
+                }
+                else if (currentBoard[knightX - 1, knightY - 2] != null && currentBoard[knightX - 1, knightY - 2].Color != knightColor)
+                {
+                    knightMoves[knightX - 1, knightY - 2] = true;
+                }
+                else
+                {
+                    knightMoves[knightX - 1, knightY - 2] = true;
+                }
+            }
+
+            // 5 = bottom left
+
+            if (knightX - 2 < 8 && knightX - 2 >= 0 && knightY - 1 >= 0 && knightY - 1 < 8)
+            {
+                if (currentBoard[knightX - 2, knightY - 1] != null && currentBoard[knightX - 2, knightY - 1].Color == knightColor)
+                {
+                    knightMoves[knightX - 2, knightY - 1] = false;
+                }
+                else if (currentBoard[knightX - 2, knightY - 1] != null && currentBoard[knightX - 2, knightY - 1].Color != knightColor)
+                {
+                    knightMoves[knightX - 2, knightY - 1] = true;
+                }
+                else
+                {
+                    knightMoves[knightX - 2, knightY - 1] = true;
+                }
+            }
+
+            // 4 = bottom right
+
+            if (knightX - 2 < 8 && knightX - 2 >= 0 && knightY + 1 >= 0 && knightY + 1 < 8)
+            {
+                if (currentBoard[knightX - 2, knightY + 1] != null && currentBoard[knightX - 2, knightY + 1].Color == knightColor)
+                {
+                    knightMoves[knightX - 2, knightY + 1] = false;
+                }
+                else if (currentBoard[knightX - 2, knightY + 1] != null && currentBoard[knightX - 2, knightY + 1].Color != knightColor)
+                {
+                    knightMoves[knightX - 2, knightY + 1] = true;
+                }
+                else
+                {
+                    knightMoves[knightX - 2, knightY + 1] = true;
+                }
+            }
+
+            // 3 = right bottom
+
+            if (knightX - 1 < 8 && knightX - 1 >= 0 && knightY + 2 >= 0 && knightY + 2 < 8)
+            {
+                if (currentBoard[knightX - 1, knightY + 2] != null && currentBoard[knightX - 1, knightY + 2].Color == knightColor)
+                {
+                    knightMoves[knightX - 1, knightY + 2] = false;
+                }
+                else if (currentBoard[knightX - 1, knightY + 2] != null && currentBoard[knightX - 1, knightY + 2].Color != knightColor)
+                {
+                    knightMoves[knightX - 1, knightY + 2] = true;
+                }
+                else
+                {
+                    knightMoves[knightX - 1, knightY + 2] = true;
+                }
+            }
+
+            // 2 = right top
+
+            if (knightX + 1 < 8 && knightX + 1 >= 0 && knightY + 2 >= 0 && knightY + 2 < 8)
+            {
+                if (currentBoard[knightX + 1, knightY + 2] != null && currentBoard[knightX + 1, knightY + 2].Color == knightColor)
+                {
+                    knightMoves[knightX + 1, knightY + 2] = false;
+                }
+                else if (currentBoard[knightX + 1, knightY + 2] != null && currentBoard[knightX + 1, knightY + 2].Color != knightColor)
+                {
+                    knightMoves[knightX + 1, knightY + 2] = true;
+                }
+                else
+                {
+                    knightMoves[knightX + 1, knightY + 2] = true;
+                }
+            }
+
+            // 1 = top right
+
+            if (knightX + 2 < 8 && knightX + 2 >= 0 && knightY + 1 >= 0 && knightY + 1 < 8)
+            {
+                if (currentBoard[knightX + 2, knightY + 1] != null && currentBoard[knightX + 2, knightY + 1].Color == knightColor)
+                {
+                    knightMoves[knightX + 2, knightY + 1] = false;
+                }
+                else if (currentBoard[knightX + 2, knightY + 1] != null && currentBoard[knightX + 2, knightY + 1].Color != knightColor)
+                {
+                    knightMoves[knightX + 2, knightY + 1] = true;
+                }
+                else
+                {
+                    knightMoves[knightX + 2, knightY + 1] = true;
+                }
+            }
+
+            return knightMoves;
         }
     }
 }
