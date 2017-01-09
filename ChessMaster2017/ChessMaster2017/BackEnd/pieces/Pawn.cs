83e9df5ff1ed615c2a218b623ca01e8467b1a312
petk0che <Cvety@CVETI>
2017-01-09 16:25:11 +0200

    Changes to ROOK and Pawn

--- ChessMaster2017/ChessMaster2017/BackEnd/pieces/Pawn.cs
+++ ChessMaster2017/ChessMaster2017/BackEnd/pieces/Pawn.cs
@@ -8,8 +8,51 @@ namespace ChessMaster2017.BackEnd
 {
     class Pawn : ChessPiece
     {
+
+        private bool hasMoved;
+
         public Pawn(int x, int y, ChessPieceColor color, ChessPieceType type) : base(x, y, color, type)
         {
+            this.hasMoved = true;
+        }
+        public override bool[,] PossibleMove(ChessPiece[,] currentBoard)
+        {
+            int pawnX = this.CurrentX;
+            int pawnY = this.CurrentY;
+
+            bool[,] pawnMoves = new bool[8, 8];
+            ChessPieceColor pawnColor = this.Color;
+
+            bool whitePawn = false;
+
+            if (this.Color == ChessPieceColor.White)
+            {
+                whitePawn = true;
+            }
+            
+
+
+         // //TOP ^
+         // for (int movesX = pawnX + 1; whitePawn && movesX >= 0 && movesX < 8; movesX++)
+         // {
+         //     int movesY = pawnY;
+         //
+         //     if (currentBoard[movesX, movesY] != null && currentBoard[movesX, movesY].Color == pawnColor)
+         //     {
+         //         pawnMoves[movesX, movesY] = false;
+         //
+         //         break;
+         //     }
+         //     else if (currentBoard[movesX, movesY] != null && currentBoard[movesX, movesY].Color != pawnColor)
+         //     {
+         //         pawnMoves[movesX, movesY] = true;
+         //
+         //         break;
+         //     }
+         //
+         // }
+
+                return pawnMoves;
         }
     }
 }
